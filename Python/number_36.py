f3f92d1021133c4a328178c5527e9536e0ae60f1
matanou <74264813+matanou@users.noreply.github.com>
2023-07-21 12:37:45 +0100

    Update number_36.py

--- Python/number_36.py
+++ Python/number_36.py
@@ -1,4 +1,6 @@
 """
+https://projecteuler.net/problem=36
+
 The decimal number, 585 = 1001001001 (binary), is palindromic in both bases.
 
 Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
